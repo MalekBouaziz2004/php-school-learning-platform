c32d692dd6968f166c7f0998746e95d4bc48e839
Malek Bouaziz <malek.bouaziz@alumni.fh-aachen.de>
2026-04-27 18:57:56 +0200

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,1 +1,18 @@
 
+<?php
+// db_connect.php - Zentrale Datenbankverbindung
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = ".";
+$db_name = "hsgg_lernzentrum";
+
+$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$link) {
+    die("Datenbankverbindung fehlgeschlagen: " . mysqli_connect_error());
+}
+
+// UTF-8 Zeichensatz setzen
+mysqli_set_charset($link, "utf8mb4");
+?>
